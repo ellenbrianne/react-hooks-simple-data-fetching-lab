e2ab5ea202bf67f938910a8106418e40422891ff
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-07-17 04:04:22 -0600

    successful fetch request using useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App () {
+
+const [dogData, setDogData] = useState("");
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(resp => resp.json())
+    .then(data => {
+        setDogData(data.message);
+    })
+}, []) 
+
+if (!dogData) {
+    return <p>Loading...</p>
+}
+
+return (
+    <>
+     <img src={dogData} alt="A Random Dog" />   
+    </>
+    )
+};
+
+
+export default App;
